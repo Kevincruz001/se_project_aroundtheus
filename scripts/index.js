3e1026cab4a6d85feb4cd225f9a52f5a302af04f
Kevin Cruz <kevincruz001@gmail.com>
2024-09-19 23:16:47 -0400

    Project 5

--- scripts/index.js
+++ scripts/index.js
@@ -39,18 +39,66 @@ const profileEditForm = profileEditModal.querySelector(".modal__form");
 const cardListEl = document.querySelector(".cards__list");
 const cardTemplate =
   document.querySelector("#card-template").content.firstElementChild;
+const addNewCardButton = document.querySelector(".profile__add-button");
+const addCardModal = document.querySelector("#add-card-modal");
+const cardModalCloseButton = document.querySelector("#card-button-close");
+const cardTitleInput = document.querySelector("#card-title-input");
+const cardLinkInput = document.querySelector("#card-link-input");
+const imageModal = document.querySelector("#image-modal");
+const imageModalCloseButton = document.querySelector(
+  "#image-modal-close-button"
+);
+const imageModalContainer = document.querySelector(".modal__image_container");
 
-function closePopup() {
+function profileClosePopup() {
   profileEditModal.classList.remove("modal_opened");
+  profileEditModal.classList.toggle("modal_closing");
+}
+
+function cardClosePopup() {
+  addCardModal.classList.remove("modal_opened");
+  addCardModal.classList.toggle("modal_closing");
+}
+
+function imageClosePopup() {
+  imageModal.classList.remove("modal_opened");
+  imageModal.classList.toggle("modal_closing");
+  setTimeout(() => {
+    imageModal.classList.remove("modal_closing");
+  }, 500);
+}
+
+function addLikeButtonListener(likeButton) {
+  likeButton.addEventListener("click", () => {
+    likeButton.classList.toggle("cards__like-button_active");
+  });
 }
 
 function getCardElement(cardData) {
   const cardElement = cardTemplate.cloneNode(true);
   const cardImageEl = cardElement.querySelector(".cards__image");
   const cardTitleEl = cardElement.querySelector(".cards__title");
+  const likeButton = cardElement.querySelector(".cards__like-button");
+  const deleteButton = cardElement.querySelector(".cards__delete-button");
+  const modalImageEl = document.querySelector(".modal__image");
+  const modalTitleEl = document.querySelector(".modal__image-title");
+
+  deleteButton.addEventListener("click", () => {
+    cardElement.remove();
+  });
+
+  cardImageEl.addEventListener("click", () => {
+    imageModal.classList.add("modal_opened");
+    modalImageEl.src = cardImageEl.src;
+    modalImageEl.alt = cardImageEl.alt;
+    modalTitleEl.textContent = cardTitleEl.textContent;
+  });
+
   cardTitleEl.textContent = cardData.name;
   cardImageEl.src = cardData.link;
   cardImageEl.alt = cardData.name;
+  addLikeButtonListener(likeButton);
+
   return cardElement;
 }
 
@@ -58,7 +106,22 @@ function handleProfileEditSubmit(e) {
   e.preventDefault();
   profileTitle.textContent = profileTitleInput.value;
   profileDescription.textContent = profileDescriptionInput.value;
-  closePopup();
+  profileClosePopup();
+}
+
+function handleCardSubmit(e) {
+  e.preventDefault();
+  const newCard = {
+    name: cardTitleInput.value,
+    link: cardLinkInput.value,
+  };
+
+  const newCardElement = getCardElement(newCard);
+
+  cardListEl.prepend(newCardElement);
+
+  cardClosePopup();
+  e.target.reset();
 }
 
 profileEditButton.addEventListener("click", function () {
@@ -66,13 +129,34 @@ profileEditButton.addEventListener("click", function () {
   profileDescriptionInput.value = profileDescription.textContent;
 
   profileEditModal.classList.add("modal_opened");
+  profileEditModal.classList.remove("modal_closing");
 });
 
-profileCloseButton.addEventListener("click", closePopup);
+profileCloseButton.addEventListener("click", profileClosePopup);
+cardModalCloseButton.addEventListener("click", cardClosePopup);
+imageModalCloseButton.addEventListener("click", imageClosePopup);
 
 profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+
+addNewCardButton.addEventListener("click", function () {
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
+
+  addCardModal.classList.add("modal_opened");
+  addCardModal.classList.remove("modal_closing");
+});
 
 initialCards.forEach((cardData) => {
   cardElement = getCardElement(cardData);
   cardListEl.prepend(cardElement);
 });
+
+const likeButtons = document.querySelectorAll(".cards__like-button");
+likeButtons.forEach((likeButton) => {
+  likeButton.addEventListener("click", () => {
+    likeButton.classList.toggle("cards__like-button_active");
+  });
+});
+
+addCardModal.addEventListener("submit", handleCardSubmit);
+likeButtons.forEach(addLikeButtonListener);
